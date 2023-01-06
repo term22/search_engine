0ec2619ae290252abab505d333df127bb358b19f
term22 <term22@outlook.com>
2023-01-06 16:53:47 -0500

    Main execution program

---
+++ main.py
@@ -0,0 +1,14 @@
+# David Burg
+# 12/6/22
+# Main Executable file
+
+# Imports
+import subprocess
+
+# Main method
+def main():
+    subprocess.call(['python', 'csc116/login.py'])
+    quit()
+
+# Execute main method
+main()
